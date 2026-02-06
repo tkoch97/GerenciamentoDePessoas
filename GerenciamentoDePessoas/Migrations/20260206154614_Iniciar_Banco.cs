913a24eb03e6a94464936523245d64ed568efa82
tkoch97 <tkoch.dev@gmail.com>
2026-02-06 13:33:27 -0300

    chore: add EntityFrameworkCoreDesign extension and initial database migration

--- GerenciamentoDePessoas/Migrations/20260206154614_Iniciar_Banco.cs
+++ GerenciamentoDePessoas/Migrations/20260206154614_Iniciar_Banco.cs
@@ -1,28 +1,31 @@
-﻿using Microsoft.EntityFrameworkCore.Migrations;
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
 
 namespace GerenciamentoDePessoas.Migrations
 {
     /// <inheritdoc />
-    public partial class Initial : Migration
+    public partial class Iniciar_Banco : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.CreateTable(
-                name: "Pessoa",
+                name: "Pessoas",
                 columns: table => new
                 {
                     Id = table.Column<int>(type: "int", nullable: false)
                         .Annotation("SqlServer:Identity", "1, 1"),
                     Nome = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: false),
                     Sobrenome = table.Column<string>(type: "nvarchar(40)", maxLength: 40, nullable: false),
-                    DataNascimento = table.Column<DateTime>(type: "datetime2", nullable: false)
+                    DataNascimento = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CPF = table.Column<string>(type: "nvarchar(11)", maxLength: 11, nullable: false),
+                    TipoSanguineo = table.Column<int>(type: "int", nullable: false)
                 },
                 constraints: table =>
                 {
-                    table.PrimaryKey("PK_Pessoa", x => x.Id);
+                    table.PrimaryKey("PK_Pessoas", x => x.Id);
                 });
         }
 
@@ -30,7 +33,7 @@ namespace GerenciamentoDePessoas.Migrations
         protected override void Down(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.DropTable(
-                name: "Pessoa");
+                name: "Pessoas");
         }
     }
 }
