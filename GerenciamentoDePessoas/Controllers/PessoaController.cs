28bce5e1f62c1bf1b26398b79d68b3e15cd29189
tkoch97 <tkoch.dev@gmail.com>
2026-02-06 14:43:06 -0300

    feat: add PessoaService dependency

--- GerenciamentoDePessoas/Controllers/PessoaController.cs
+++ GerenciamentoDePessoas/Controllers/PessoaController.cs
@@ -1,12 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using GerenciamentoDePessoas.Models;
+using GerenciamentoDePessoas.Services;
+using Microsoft.AspNetCore.Mvc;
 
 namespace GerenciamentoDePessoas.Controllers
 {
     public class PessoaController : Controller
     {
-        public IActionResult Index()
+        private readonly IPessoaService _pessoaService;
+
+        public PessoaController(IPessoaService pessoaService)
         {
-            return View();
+            _pessoaService = pessoaService;
+        }
+
+        public async Task<ActionResult<List<Pessoa>>> Index()
+        {
+            var pessoaServiceResponse = await _pessoaService.BuscarTodos();
+            return View(pessoaServiceResponse);
         }
     }
 }
